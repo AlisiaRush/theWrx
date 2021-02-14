e7d77de1f811190ee7bb53bdad022f2748aac27e
fmaslim <franky.maslim@yahoo.com>
2021-02-14 18:11:32 -0500

    Added Routing on parent and child components

--- theWrx/src/app/app-routing.module.ts
+++ theWrx/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { UserAdministrationComponent } from './user-administration/user-administration.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HelpComponent } from './help/help.component';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserAddNewComponent } from './user-add-new/user-add-new.component';
+import { UserDeleteComponent } from './user-delete/user-delete.component';
+import { UserUpdateComponent } from './user-update/user-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-administration', component: UserAdministrationComponent, children: [
+    { path: 'add', component: UserAddNewComponent },
+    { path: 'delete', component: UserDeleteComponent },
+    { path: 'update', component: UserUpdateComponent },
+  ]},
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'help', component: HelpComponent },
+  { path: '', component: AppComponent },
+  { path: '**', component: PageNotFoundComponent }, // to handle unrecognized paths
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- theWrx/src/app/app.module.ts
+++ theWrx/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserAdministrationComponent } from './user-administration/user-administration.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HelpComponent } from './help/help.component';
+import { UserAddNewComponent } from './user-add-new/user-add-new.component';
+import { UserUpdateComponent } from './user-update/user-update.component';
+import { UserDeleteComponent } from './user-delete/user-delete.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAdministrationComponent,
+    ContactUsComponent,
+    HelpComponent,
+    UserAddNewComponent,
+    UserUpdateComponent,
+    UserDeleteComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
