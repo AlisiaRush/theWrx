043fb361360738c6d15f50ae0b7ca5d883b15741
Alisia Rush <alisiarush@gmail.com>
2021-02-16 19:15:36 -0500

    Added Routing, created child routing from User Admin Component. Set redirect to Add New User.

--- theWrx/src/app/app-routing.module.ts
+++ theWrx/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserAdminComponent } from './components/user-admin/user-admin.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HelpComponent } from './components/help/help.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+import { AddNewUserComponent } from './components/add-new-user/add-new-user.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { DeleteUserComponent } from './components/delete-user/delete-user.component';
+
+
+const routes: Routes = [
+  { path: 'home', component:HomeComponent},
+  { path: 'user-admin', component:UserAdminComponent,
+  children: [
+    { path: '', redirectTo: 'add-new-user', pathMatch: 'full' },
+    { path: 'add-new-user', component: AddNewUserComponent },
+    { path: 'update-user', component: UpdateUserComponent },
+    { path: 'delete-user', component: DeleteUserComponent }
+  ]
+},
+  { path: 'contact-us', component:ContactUsComponent},
+  { path: 'help', component:HelpComponent},
+  { path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- theWrx/src/app/app.module.ts
+++ theWrx/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserAdminComponent } from './components/user-admin/user-admin.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HelpComponent } from './components/help/help.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { DeleteUserComponent } from './components/delete-user/delete-user.component';
+import { AddNewUserComponent } from './components/add-new-user/add-new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAdminComponent,
+    ContactUsComponent,
+    HelpComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    UpdateUserComponent,
+    DeleteUserComponent,
+    AddNewUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
